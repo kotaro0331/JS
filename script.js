f817e37a1a4d32993dbb907ea37bc6fb3d792e8d
kotaro0331 <k2yehjp@yahoo.co.jp>
2023-09-18 17:39:00 +0900

    学習内容
    コンソールでのイベント確認

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-window.alert("ブラウザオブジェクトの取得に成功！")
+var lists=  'list1' 
+document.getElementById("lists")
+
+function getElement(){
+    element = document.querySelector('#shopB');
+    console.log('選ばれたのは' + element.textContent +'でした');
+}
+
+function getElement(){
+    elements = document.querySelectorAll('.shop li');
+
+    let len  = elements.length;
+    for (let i = 0; i < len; i++){
+        console.log(elements.item(i).textContent);
+    }
+}
