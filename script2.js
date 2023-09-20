ce10e8253e0e4eac188a0f14cb97c96ef3391ceb
kotaro0331 <k2yehjp@yahoo.co.jp>
2023-09-20 21:56:50 +0900

    イベント発火基礎編
    イベント発火

--- script2.js
+++ script2.js
@@ -1,2 +1,29 @@
 let lists = document.getElementsByClassName("pull-down-list")
 console.log(lists)
+
+
+window.addEventListener('load',function(){
+const pullDownButton = document.getElementById("lists")
+})
+
+
+window.addEventListener('load',function(){
+
+    document.getElementById("lists").addEventListener('mouseenter',logOn);
+    document.getElementById("lists").addEventListener('mouseleave',logOff);
+});
+
+function logOn(event){
+    console.log("乗る");
+}
+
+function logOff(event){
+    console.log("外れる");
+}
+
+
+window.addEventListener('load',function(){
+    document.getElementById("lists").addEventListener('click',function()
+    {console.log("クリック");
+});
+});
