6f828a83e5d51aedfb3f2b69f1d86f83cbe226de
kotaro0331 <k2yehjp@yahoo.co.jp>
2023-09-21 21:48:05 +0900

    イベント発火基礎編
    イベント発火（インラインスタイル）

--- script2.js
+++ script2.js
@@ -1,29 +1,38 @@
 let lists = document.getElementsByClassName("pull-down-list")
 console.log(lists)
 
+const pullDownButton = document.getElementById("lists")
+pullDownButton.addEventListener('mouseover',function(){
+    pullDownButton.setAttribute("style","background-color: blue;");
+  })
+
+pullDownButton.addEventListener('mouseout',function(){
+    pullDownButton.setAttribute("style","background-color: red;");
+  })
+
+pullDownButton.addEventListener('click',function(){
+    pullDownButton.setAttribute("style","background-color: green;");
+  })
 
 window.addEventListener('load',function(){
-const pullDownButton = document.getElementById("lists")
+
+    document.getElementById("lists").addEventListener('mouseover',logOn);
+    document.getElementById("lists").addEventListener('mouseout',logOff);
+    
 })
 
-
-window.addEventListener('load',function(){
-
-    document.getElementById("lists").addEventListener('mouseenter',logOn);
-    document.getElementById("lists").addEventListener('mouseleave',logOff);
-});
-
 function logOn(event){
-    console.log("乗る");
+    console.log("乗る青");
 }
 
 function logOff(event){
-    console.log("外れる");
+    console.log("外れる赤");
 }
 
 
 window.addEventListener('load',function(){
     document.getElementById("lists").addEventListener('click',function()
-    {console.log("クリック");
+    {console.log("クリック緑");
 });
 });
+
